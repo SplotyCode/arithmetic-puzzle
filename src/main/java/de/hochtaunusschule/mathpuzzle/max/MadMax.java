b50ee268f085ed3139c06b40d2a3eea67158aaa4
splotycode <davidscandurra@gmail.com>
2022-04-24 21:57:38 +0200

    Improve MadMax

--- src/main/java/de/hochtaunusschule/mathpuzzle/max/MadMax.java
+++ src/main/java/de/hochtaunusschule/mathpuzzle/max/MadMax.java
@@ -1,93 +1,76 @@
 package de.hochtaunusschule.mathpuzzle.max;
 
-import com.google.common.primitives.ImmutableIntArray;
-import java.util.Arrays;
 import lombok.AllArgsConstructor;
-import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * @author David (_Esel)
  */
 @AllArgsConstructor
 public class MadMax implements HexBinary {
-    private static int[] start;
-
-    private static boolean checkDowngrade(int[] input) {
-        for (int i = 0; i < start.length; i++) {
-            if (start[i] > input[i]) {
-                return true;
-            }
-        }
-        return false;
+    public static String solve(String input, int maxMoves) {
+        int[] result = solve(HexBinary.extractHex(input), maxMoves);
+        String asString = HexBinary.toHexString(result);
+        System.out.println("Result for " + input + " is " + asString);
+        return asString;
     }
 
     public static int[] solve(int[] input, int maxSteps) {
-        System.out.println("total: " + HexCache.totalLetterSize(input));
-        return go(input, input.length, HexCache.totalLetterSize(input), maxSteps, 0);
+        if (maxSteps < 0) {
+            throw new IllegalArgumentException("maxSteps might not be negative");
+        }
+        return solve(input, 0, input.length, HexCache.totalLetterSize(input), maxSteps, 0);
     }
 
-    public static int[] go(int[] input, int length, int remaining, int move, int different) {
-        //System.out.println("check " + move + " " + different);
-        if (move < 0) {
-          //  System.out.println("#1");
+    private static int[] solve(int[] input, int offset, int length, int remaining, int moves, int reserved) {
+        if (moves < 0) { /* moves exhausted */
             return null;
         }
-        if (input.length == 0) {
-         //   System.out.println("#2");
-            return different == 0 ? input : null;
+        if (input.length - offset == 0) { /* end of tree */
+            /* check that we have no reserved blades left */
+            return reserved == 0 ? input : null;
         }
-        if (move == 0 && different == 0) {
-           // System.out.println("#3");
-            return input;
+        if (moves == 0) { /* we are not in the tail but have no moves left */
+            /* if we have no reserved/left over blades we are fine */
+            return reserved == 0 ? input : null;
         }
-        if (move == 0) {
-           // System.out.println("#4");
+        int maxBlades = length * 7;
+        if (reserved < 0 && Math.abs(reserved) > maxBlades - remaining) { /* put more than place */
             return null;
         }
-        if (different < 0 && Math.abs(different) > move) {
-           // System.out.println("#5");
+        if (reserved < 0 && Math.abs(reserved) > moves) { /* we can not for-fill reserve as the moves would not be enough  */
             return null;
         }
         int minBlades = length * 2;
-        /*if (remaining - minBlades < different) {
-            return null;
-        }*/
-        if (different < 0 && remaining - Math.abs(different) < minBlades) {
-        //    System.out.println("#6");
+        if (remaining < reserved || remaining - minBlades < reserved) { /* reserve more than we have left */
             return null;
         }
-        int[] copy = ArrayUtils.remove(input, 0);
-        System.out.println("go " + HexBinary.toHexString(input) + " " + length + " " + remaining + " " + move + " " + different);
+        if (reserved < 0 && remaining - Math.abs(reserved) < minBlades) { /* when serving reserves we could not  */
+            return null;
+        }
+        int childRemaining = remaining - HexCache.letterSize(input[offset]);
         for (int upgrade = DIGITS.length - 1; upgrade >= 0; upgrade--) {
-            HexMove.Move costs = HexCache.move(input[0], upgrade);
-            System.out.println("go1 " + upgrade + " " + costs.moves + " " + costs.fromOutside + " " +
-                Arrays.toString(copy) + " " + length + " " + remaining + " " + HexCache.letterSize(input[0]) + " " + move + " " + different);
-            int[] res = go(copy, length - 1, remaining - HexCache.letterSize(input[0]), move - costs.moves, different + costs.fromOutside);
-            res = res == null ? null : ImmutableIntArray.builder().add(upgrade).addAll(res).build().toArray();
-            System.out.println("go3 " + Arrays.toString(res)
-                + " " + upgrade
-                + " "
-                + costs.moves
-                + " "
-                + costs.fromOutside + " "
-                + Arrays.toString(copy)
-                + " " + length
-                + " " + remaining + " " + HexCache.letterSize(input[0]) + " " + move + " " + different);
-            System.out.println("go2 " + Arrays.toString(res));
+            HexMove.Move costs = HexCache.move(input[offset], upgrade);
+            int[] res = solve(
+                input,
+                offset + 1, length - 1, childRemaining,
+                moves - costs.moves, reserved + costs.fromOutside
+            );
             if (res != null) {
+                res[offset] = upgrade;
                 return res;
             }
         }
-        System.out.println("go2 Nothing");
         return null;
     }
 
-    public static void main(String[] args) {
-        System.out.println(HexBinary.toHexString(solve(HexBinary.extractHex("1A02B6B50D7489D7708A678593036FA265F2925B21C28B4724DD822038E3B4804192322F230AB7AF7BDA0A61BA7D4AD8F888"), 87)));
-        //System.out.println(new BigInteger("EF50AA77ECAD25F5E11A307B713EAAEC55215E7E640FD263FA529BBB48DC8FAFE14D5B02EBF792B5CCBBE9FA1330B867E330A6412870DD2BA6ED0DBCAE553115C9A31FF350C5DF993824886DB5111A83E773F23AD7FA81A845C11E22C4C45005D192ADE68AA9AA57406EB0E7C9CA13AD03888F6ABEDF1475FE9832C66BFDC28964B7022BDD969E5533EA4F2E4EABA75B5DC11972824896786BD1E4A7A7748FDF1452A5079E0F9E6005F040594185EA03B5A869B109A283797AB31394941BFE4D38392AD12186FF6D233585D8C820F197FBA9F6F063A0877A912CCBDCB14BEECBAEC0ED061CFF60BD517B6879B72B9EFE977A9D3259632C718FBF45156A16576AA7F9A4FAD40AD8BC87EC569F9C1364A63B1623A5AD559AAF6252052782BF9A46104E443A3932D25AAE8F8C59F10875FAD3CBD885CE68665F2C826B1E1735EE2FDF0A1965149DF353EE0BE81F3EC133922EF43EBC09EF755FBD740C8E4D024B033F0E8F3449C94102902E143433262CDA1925A2B7FD01BEF26CD51A1FC22EDD49623EE9DEB14C138A7A6C47B677F033BDEB849738C3AE5935A2F54B99237912F2958FDFB82217C175448AA8230FDCB3B3869824A826635B538D47D847D8479A88F350E24B31787DFD60DE5E260B265829E036BE340FFC0D8C05555E75092226E7D54DEB42E1BB2CA9661A882FB718E7AA53F1E606", 16));
-        //int[] input = HexBinary.extractHex("D24");
-        //start = input.clone();
-        //solve(input, 0, 0, 0, 3);
-        //System.out.println(Integer.parseInt("EE4", 16));
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) {
+            System.out.println("Illegal usage: Use <hex number> <max moves>");
+            return;
+        }
+        long start = System.currentTimeMillis();
+        solve(args[0], Integer.parseInt(args[1]));
+        System.out.println((System.currentTimeMillis() - start));
+
     }
 }
--- src/test/java/de/hochtaunusschule/mathpuzzle/max/HexBinaryTest.java
+++ src/test/java/de/hochtaunusschule/mathpuzzle/max/HexBinaryTest.java
@@ -1,7 +1,6 @@
 package de.hochtaunusschule.mathpuzzle.max;
 
-import com.google.common.primitives.ImmutableIntArray;
-import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -18,21 +17,25 @@ public class HexBinaryTest {
     }
 
     @Test
-    void test2() {
-        //0xE4 0xE 1 0 0x4 2 9 5 1 0
-        //[4]  14  1 0 [4] 2 9 5 1 0
-        System.out.println(Arrays.toString(MadMax.go(
-            new int[] {4},
-            2 - 1,
-            9 - 5,
-            1 - 1,
-            0 + 0
-        )));
+    void edgeCases() {
+        Assertions.assertEquals(MadMax.solve("11111111", 300), "11111111");
+        Assertions.assertEquals(MadMax.solve("EEEE11111111", 300), "ffe811111111");
+        Assertions.assertEquals(MadMax.solve("EEEE11111111", 0), "eeee11111111");
     }
 
     @Test
-    void add() {
-        System.out.println(Arrays.toString(
-            ImmutableIntArray.builder().add(1).addAll(new int[] {2, 3}).build().toArray()));
+    void testProphet() {
+        Assertions.assertEquals(
+            HexBinary.toHexString(HexBinary.extractHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF88EFA9EBE89EFA99FBDAA8E8EAD88AB899F8E8F9AA9E9AD88988EDA9A99888EDAD989A8BAFD8A888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888")),
+            "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff88efa9ebe89efa99fbdaa8e8ead88ab899f8e8f9aa9e9ad88988eda9a99888edad989a8bafd8a888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888"
+        );
+    }
+
+    @Test
+    void bwInf() {
+        Assertions.assertEquals(
+            MadMax.solve("1A02B6B50D7489D7708A678593036FA265F2925B21C28B4724DD822038E3B4804192322F230AB7AF7BDA0A61BA7D4AD8F888", 87),
+            "ffffffffffffffffffffffffffffffffffffffeb8de88baa8add888898e9ba88ad98988f898ab7af7bda8a61ba7d4ad8f888"
+        );
     }
 }
